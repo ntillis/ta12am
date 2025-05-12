a03c380aa37089c61181f5cab08d55eaaaa7eecb
ntillis <68442958+ntillis@users.noreply.github.com>
2025-05-11 20:08:38 -0400

    added formatting to posts

--- components/PostViewer.tsx
+++ components/PostViewer.tsx
@@ -1,36 +1,56 @@
 "use client";
 
 import { Post } from "@/generated/prisma";
-import Script from 'next/script'
+import Script from "next/script";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 interface PostViewerProps {
   post: Post;
 }
 
 export default function PostViewer({ post }: PostViewerProps) {
-  const url = `/posts/${post.slug}`;
+  const time = post.createdAt.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit'
+  })
   return (
     <>
-    <Script 
-      src='https://platform-api.sharethis.com/js/sharethis.js#property=65b2921ab492fb00132dd19e&product=inline-share-buttons' 
-      async={true} 
-      strategy="afterInteractive"
-    />
-    <div className="relative flex flex-col m-6 p-10 w-full max-w-4xl mx-auto z-10">
-      <header className="w-full flex flex-col items-center border-b">
-        <h1 className="text-3xl font-bold mb-4 text-center">{post.title}</h1>
-        <div className="pt-3 pb-6 text-gray-600 flex w-full items-center justify-end">
-          <h2 className="">by {post.author} on {post.createdAt.toLocaleDateString('en-US', {
-                    weekday: 'short',
-                    year: 'numeric',
-                    month: 'short',
-                    day: 'numeric'
-                })}</h2>
-        </div>
-      </header>
-      <div className="sharethis-inline-share-buttons mt-5 pt-2 relative z-10"></div>
-      <div className="my-10 mx-auto max-w-2xl" dangerouslySetInnerHTML={{ __html: post.content }} />
-    </div>
+      <Script
+        src="https://platform-api.sharethis.com/js/sharethis.js#property=65b2921ab492fb00132dd19e&product=inline-share-buttons"
+        async={true}
+        strategy="afterInteractive"
+      />
+      <div className="relative flex flex-col m-6 p-10 w-full max-w-4xl mx-auto z-10">
+        <header className="w-full flex flex-col border-b">
+          <h1 className="text-3xl font-bold mb-4 w-full px-5 text-left">
+            {post.title}
+          </h1>
+          <div className="flex justify-between items-center">
+            <div className="pt-3 pb-6 px-5 text-gray-600 flex flex-col w-full">
+              <h2 className="text-lg font-semibold">{post.author}</h2>
+              <h2 className="text-sm">
+                {post.createdAt.toLocaleDateString("en-US", {
+                  weekday: "short",
+                  year: "numeric",
+                  month: "short",
+                  day: "numeric",
+                })} AT {time}
+              </h2>
+            </div>
+            <div className="px-5">
+                <Avatar className="w-12 h-12">
+                  <AvatarImage src="/pfp.png"/>
+                  <AvatarFallback>MT</AvatarFallback>
+                </Avatar>
+            </div>
+          </div>
+        </header>
+        <div className="sharethis-inline-share-buttons mt-5 pt-2 relative z-10"></div>
+        <div
+          className="my-10 mx-auto max-w-2xl"
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
+      </div>
     </>
   );
 }
